91e3cf47c769f7ba26f6ce018b816e5f04ad9fce
Rakely Dias <rakelyyasmim@gmail.com>
2025-10-22 17:36:34 -0300

    commit

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ const port = 3333
 // GET, POST, PATCH, PUT, DELETE
 
 app.use(cors())
+app.use(express.json())
 
 app.get("/", (request, response) => {
     response.json(persons)
+})
+
+app.post("/cadastrar", (request,response) => {
+    //desestruturação
+    const { user } = request.body
+    console.log(user)
+
+    //cadastro do banco de dados
+
+    response.status(201).json({ message: "Usuário cadastrado com sucesso! "})
 })
 
 app.listen(port, () => {
